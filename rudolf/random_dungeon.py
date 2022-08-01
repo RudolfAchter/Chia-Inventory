222bd2a25d8da9a97c18d8be0ab88e7998fe8c37
Rudolf Achter <rudolf.achter@uni-passau.de>
2022-08-01 21:57:22 +0200

    update: minor better dungeon generation

--- rudolf/random_dungeon.py
+++ rudolf/random_dungeon.py
@@ -33,23 +33,42 @@ def random_dungeon(location,location_file):
     
     #just initialize opposite direction with non direction
     odir=5
+    cdir=0
     
     duda[x][y]=ed
     duda[x][y]["name"]=location
     prev_room_name=location
     
-    
+    noreturn=data["dungeon_settings"]["noreturn"]
+
     for i in range(data["dungeon_settings"]["steps"]):
         print("room " + str(i))
         #possible directions
         pdir=[]
         for dir in directions:
             #print("direction: " + directions[dir]["name"])
-            if (y + directions[dir]["y"]) in range(data["dungeon_settings"]["height"]) and ( #not out of y coordinates
-                x + directions[dir]["x"]) in range(data["dungeon_settings"]["width"]) and (  #not out of x coordinates
-                dir != odir # not in opposite direction
-                ):
-                pdir.append(dir)
+            ny=y + directions[dir]["y"]
+            nx=x + directions[dir]["x"]
+            if (ny) in range(data["dungeon_settings"]["height"]) and ( #not out of y coordinates
+                nx) in range(data["dungeon_settings"]["width"])  and ( #not out of x coordinates
+                dir != odir ):# not in opposite direction
+                doappend=True
+                
+                #Check if Room already exists
+                if nx in duda:
+                    if ny in duda[nx] and len(pdir) > 0 and noreturn <=0: #allow direction if there is no other possible direction (would end in error then)
+                        doappend=False #if Room already exists do not go this direction
+                    else:
+                        noreturn=data["dungeon_settings"]["noreturn"] #reset noreturn
+                if doappend:
+                    #more weight for same direction
+                    if dir == cdir:
+                        for j in range(data["dungeon_settings"]["samedirection"]):
+                            pdir.append(dir) #append multiple times
+                    else:
+                        pdir.append(dir)
+        noreturn-=1
+                
         print("possible directions: " +','.join(pdir))
         #print("possible directions len: " + str(len(pdir)))
         #chosen direction
@@ -93,6 +112,12 @@ def random_dungeon(location,location_file):
                     "file" : location_file
                 }
             }
+
+        #save number of step for showing in debug render
+        if 'stepnr' in duda[x][y]:
+            duda[x][y]['stepnr'].append(i)
+        else:
+            duda[x][y]['stepnr']=[i]
     
     #Dumping and writing File is for testing purposes now
     #want to visually see if dungeon makes sense
@@ -109,18 +134,26 @@ def random_dungeon(location,location_file):
                 background: black;
             }
             table {
-                width:1000px;
-                height:1000px;
                 border-spacing: 1px;
                 /*border-collapse: collapse;*/
+            }
+            td{
+                height:100px;
+                width:100px;
             }
             td.block {
                 border: 20px solid black;
                 background-color: #111111;
+                height:100px;
+                width:100px;
+
             }
             td.room {
                 border: 20px solid black;
                 background-color: #cccccc;
+                height:100px;
+                width:100px;
+
             }
             td.north {
                 border-top: 20px solid #cccccc;
@@ -147,10 +180,16 @@ def random_dungeon(location,location_file):
                 for key in duda[x][y]:
                     if key in ["north","south","east","west"]:
                         classes.append(key)
+                #cell content
+                if 'stepnr' in duda[x][y]:
+                    cont=str(duda[x][y]['stepnr'])
             else:
                 classes=['block']
+                cont=""
             out+='<td class="' + ' '.join(classes) + '">'
+            out+=cont
             out+='</td>'
+            
         out+='</tr>'
     out+='</table>'
     
