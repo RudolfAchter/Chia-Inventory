bfa655f3cd803e9685262d6c35849802f9cd57ec
Rudolf Achter <rudolf.achter@gmail.com>
2022-08-01 07:40:52 +0200

    items_by_data_first version

---
+++ rudolf/items_by_data.py
@@ -0,0 +1,12 @@
+# Just Testing This is definitely not THE code :-D
+
+item_dict = {}
+
+item_dict['Weapon'] = {}
+
+item_dict['Weapon']['Knife']=[
+    'Knife 01', 'Knife 02', 'Knife 03', 'Knife 04'
+]
+
+item_dict['Weapon']['Sword']
+
